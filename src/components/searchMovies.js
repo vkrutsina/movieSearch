ea045197d0e0515a5f2cf080cd460549836a80f5
vkrutsina <vera.krutsina@gmail.com>
2020-08-28 15:43:04 -0700

    movie cards

--- src/components/searchMovies.js
+++ src/components/searchMovies.js
@@ -1,36 +1,70 @@
-import React from 'react';
+/* eslint-disable react/jsx-no-comment-textnodes */
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useState } from 'react';
 
 export default function searchMovies() {
+  const [query, setQuery] = useState('');
+
+  const [movies, setMovies] = useState([]);
+
   const searchMovies = async (e) => {
     e.preventDefault();
 
     const key = process.env.REACT_APP_TMDB_API_KEY;
 
-    let query = 'Jurassic';
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=${query}&page=1&include_adult=false`;
+
     try {
       const res = await fetch(url);
       const data = await res.json();
-      console.log(data);
+      setMovies(data.results);
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <form className="form" onSubmit={searchMovies}>
-      <label htmlFor="query" className="label">
-        Movie Name
-      </label>
-      <input
-        type="text"
-        className="input"
-        name="query"
-        placeholder="search here"
-      ></input>
-      <button className="button" type="submit">
-        Search
-      </button>
-    </form>
+    <>
+      <form className="form" onSubmit={searchMovies}>
+        <label htmlFor="query" className="label">
+          Movie Name
+        </label>
+        <input
+          type="text"
+          className="input"
+          name="query"
+          placeholder="search here"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        ></input>
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="card-list">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <div className="card" key={movie.id}>
+              <img
+                className="card--image"
+                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                alt={movie.title + ' poster'}
+              />
+              <div className="cart--content">
+                <h3 className="card--title">{movie.title}</h3>
+                <p>
+                  <small>RELEASE DATE: {movie.release_date}</small>
+                </p>
+                <p>
+                  <small>RATING: {movie.vote_average}</small>
+                </p>
+                <p class="card-desc">{movie.overview}</p>
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
   );
 }
