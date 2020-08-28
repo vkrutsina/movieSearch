812351c56fd7ce6f1d07a05b7f077e6b90445abf
vkrutsina <vera.krutsina@gmail.com>
2020-08-28 15:11:38 -0700

    with external api

--- src/components/searchMovies.js
+++ src/components/searchMovies.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
 export default function searchMovies() {
+  const searchMovies = async (e) => {
+    e.preventDefault();
+
+    const key = process.env.REACT_APP_TMDB_API_KEY;
+
+    let query = 'Jurassic';
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={searchMovies}>
       <label htmlFor="query" className="label">
         Movie Name
       </label>
