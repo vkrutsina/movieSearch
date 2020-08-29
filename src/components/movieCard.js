0f8a9bb6ae06e884de1a540db9a56ef0bacf8a62
Vera Krutsina <vera.krutsina@gmail.com>
2020-08-29 16:41:37 -0700

    like button

--- src/components/movieCard.js
+++ src/components/movieCard.js
@@ -1,8 +1,21 @@
-import React from 'react';
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, {useState} from 'react';
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function movieCard({ movie }) {
+  const [button, setButton] = useState('gray');
+
+  const buttonChange = (e) => {
+    if(button === 'gray') setButton('#FE938C'); 
+    if(button === '#FE938C') setButton('gray');
+  }
+
   return (
+    
     <div className="card" key={movie.id}>
+      <FontAwesomeIcon className='like-icon' icon={faHeart} style={{color: button}} onClick={buttonChange} />
+
       <img
         className="card--image"
         src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
@@ -16,8 +29,10 @@ export default function movieCard({ movie }) {
         <p>
           <small>RATING: {movie.vote_average}</small>
         </p>
-        <p class="card-desc">{movie.overview}</p>
+        <p className="card-desc">{movie.overview}</p>
       </div>
     </div>
   );
 }
+
+
